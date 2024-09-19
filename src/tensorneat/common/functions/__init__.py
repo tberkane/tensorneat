189ddae65b62aa6d054fa2b3b4edd823913c97e5
Thomas Berkane <thomas@berkane.fr>
2024-09-19 15:02:06 -0400

    fix

--- src/tensorneat/common/functions/__init__.py
+++ src/tensorneat/common/functions/__init__.py
@@ -82,14 +82,13 @@ def apply_aggregation(idx, z, agg_funcs):
     """
     idx = jnp.asarray(idx, dtype=jnp.int32)
 
+    def agg_wrapper(x):
+        return jax.lax.switch(idx, agg_funcs, x)
+
     return jax.lax.cond(
         jnp.all(jnp.isnan(z)),
-        lambda: jnp.full_like(
-            z, jnp.nan
-        ),  # Return an array of NaNs with the same shape as z
-        lambda: jax.lax.switch(
-            idx, agg_funcs, z
-        ),  # Apply aggregation to the entire input
+        lambda: jnp.full(z.shape[:-1], jnp.nan),  # Return NaNs with shape (50,)
+        lambda: jax.vmap(agg_wrapper)(z),  # Apply aggregation to each row
     )
 
 
